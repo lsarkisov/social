717a2f40e9875234f4a66d1fd3e354c823c65c70
coder <coder@coders-MacBook-Pro.local>
2020-03-26 13:30:31 +0100

    show icon in navbar

--- src/components/auth/navbar/links.js
+++ src/components/auth/navbar/links.js
@@ -3,19 +3,25 @@ import { Link } from 'react-router-dom'
 
 export default function AuthNavbarLinks() {
   return (
-    <div className="auth-navbar__link">
-      <Link to="/" className="bounceOutLeft">
-        About
-      </Link>
-      <Link to="/" className="bounceOutLeft">
-        Faq
-      </Link>
-      <Link to="/" className="bounceOutLeft">
-        Contact
-      </Link>
-      <Link to="/" className="bounceOutLeft">
-        How it works
-      </Link>
-    </div>
+    <>
+      <div className="auth-navbar__icon">
+        <span>i</span>
+        <span>i</span>
+      </div>
+      <div className="auth-navbar__link">
+        <Link to="/" className="bounceOutLeft">
+          About
+        </Link>
+        <Link to="/" className="bounceOutLeft">
+          Faq
+        </Link>
+        <Link to="/" className="bounceOutLeft">
+          Contact
+        </Link>
+        <Link to="/" className="bounceOutLeft">
+          How it works
+        </Link>
+      </div>
+    </>
   )
 }
