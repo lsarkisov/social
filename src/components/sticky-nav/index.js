57140e9d8bc59aa38e1d1be0e22f13a7e15a247a
coder <coder@coders-MacBook-Pro.local>
2020-04-08 18:11:15 +0200

    components for: * sticky nav * pattern for main page

---
+++ src/components/sticky-nav/index.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react'
+
+export default function StickyNav(props) {
+  const [show, setShow] = useState(false)
+  const delta = 50
+
+  useEffect(() => {
+    window.addEventListener('scroll', (e) => {
+      if (document.documentElement.scrollTop > window.innerHeight - delta) {
+        setShow(true)
+      } else {
+        setShow(false)
+      }
+    })
+  })
+  return (
+    <nav className={`sticky-nav ${show ? 'sticky-nav__show' : ''}`}>
+      {props.children}
+    </nav>
+  )
+}
