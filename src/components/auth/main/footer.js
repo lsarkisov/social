3df5966fe289ee49bbade9c5edc417096326b3b3
coder <coder@coders-MacBook-Pro.local>
2020-04-10 18:34:57 +0200

    removed unused components

--- src/components/auth/main/footer.js
+++ src/components/auth/main/footer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
 import Logo from 'components/logo'
 
 export default function AuthMainFooter(props) {
