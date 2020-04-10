c86f2323505116d6af833949de3ffdbd8101d008
coder <coder@coders-MacBook-Pro.local>
2020-04-10 18:11:14 +0200

    updated shadows on main page

--- src/components/auth/main/content.js
+++ src/components/auth/main/content.js
@@ -1,49 +1,16 @@
-import React, { useState, useRef } from 'react'
+import React from 'react'
 import { Container } from 'react-bootstrap'
 import AuthMainAbout from 'components/auth/main/about'
 import AuthMainHowItWorks from 'components/auth/main/how-it-works'
 import AuthMainCustomers from 'components/auth/main/customers'
 
 export default function AuthMainContent(props) {
-  const content = useRef(null)
-  const [height, setHeight] = useState(null)
-  const [width, setWidth] = useState(null)
-
-  useState(() => {
-    setTimeout(() => {
-      setHeight(content.current.offsetHeight)
-      setWidth(content.current.offsetWidth)
-    })
-    window.addEventListener('resize', () => {
-      setHeight(content.current.offsetHeight)
-      setWidth(content.current.offsetWidth)
-    })
-  }, [content])
-
   return (
-    <div className="main-page__content" ref={content}>
-      <i
-        className="bg bg-1"
-        style={{
-          width: `${Math.round(width / 3)}px`,
-          borderTopWidth: `${height}px`,
-        }}
-      />
-      <i
-        className="bg bg-2"
-        style={{
-          width: `${Math.round(width / 3)}px`,
-          borderTopWidth: `${height}px`,
-        }}
-      />
+    <div className="main-page__content">
+      <i className="bg bg-1" />
+      <i className="bg bg-2" />
+      <i className="bg bg-3" />
 
-      <i
-        className="bg bg-3"
-        style={{
-          width: `${Math.round(width / 4)}px`,
-          borderBottomWidth: `${height}px`,
-        }}
-      />
       <Container>
         <AuthMainAbout />
         <AuthMainHowItWorks />
