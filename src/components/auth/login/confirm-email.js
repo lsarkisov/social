444604c22c875f3ddc9ad0daec7534e505b35788
coder <coder@coders-MacBook-Pro.local>
2020-04-14 20:00:19 +0200

    congratulation page

--- src/components/auth/login/confirm-email.js
+++ src/components/auth/login/confirm-email.js
@@ -1,31 +1,29 @@
 import React from 'react'
 import { Form, Button } from 'react-bootstrap'
 import { useTranslation } from 'react-i18next'
-import AuthContent from 'components/auth/content'
-import AuthForm from 'components/auth/form'
+import Congratulation from 'components/congratulation'
+import DoubleText from 'components/double-text'
 
 export default function AuthLoginConfirmEmail(props) {
   const { t } = useTranslation()
 
   return (
-    <AuthContent>
-      <AuthForm>
-        <h2 className="title-lg">{t('login.confirm.title')}</h2>
-        <p className="auth-login__subtitle-extra">
-          {t('login.confirm.message1')}
-        </p>
-        <p className="auth-login__subtitle-extra">
-          {t('login.confirm.message2')}
-        </p>
-        <p className="auth-login__subtitle-extra">
-          {t('login.confirm.message3')}
-        </p>
-        <Form>
-          <Form.Group className="auth-login__congrat-button">
-            <Button variant="primary">{t('login.confirm.button')} </Button>
-          </Form.Group>
-        </Form>
-      </AuthForm>
-    </AuthContent>
+    <Congratulation className="confirm">
+      <h2 className="title-lg">
+        <DoubleText text={t('login.confirm.title')} />
+      </h2>
+      <p className="confirm__subtitle-extra">{t('login.confirm.message1')}</p>
+      <p className="confirm__text">{t('login.confirm.message2')}</p>
+      <p className="confirm__text">{t('login.confirm.message3')}</p>
+      <p className="confirm__text">{t('login.confirm.message4')}</p>
+      <p className="confirm__text">{t('login.confirm.message5')}</p>
+      <Form>
+        <Form.Group className="confirm__button">
+          <Button size="lg" variant="primary">
+            {t('login.confirm.button')}{' '}
+          </Button>
+        </Form.Group>
+      </Form>
+    </Congratulation>
   )
 }
